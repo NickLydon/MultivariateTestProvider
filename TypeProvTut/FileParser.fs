472805cdbb253c41676611991f75f236138180b7
NicholasL <nickkell@gmail.com>
2015-05-05 10:50:44 +0100

    Init

---
+++ TypeProvTut/FileParser.fs
@@ -0,0 +1,42 @@
+﻿module FileParser
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+type private Name = string
+type private Weight = int
+type private Variant = Name * Weight
+type private Test = Name * Variant list
+
+let parseFile file =
+
+    let rec loop accumulatedTests currentTest remainingLines : Test list =
+
+        let parseVariant (variant:string) =
+            let [| name; weight; |] = 
+                variant.Split([| ":" |], StringSplitOptions.RemoveEmptyEntries) 
+                |> Array.map(fun s -> s.Trim())
+
+            (name,int(weight))
+
+        match remainingLines with
+        | [] -> 
+            match currentTest with
+            | None -> accumulatedTests
+            | Some current -> current::accumulatedTests
+        | head::tail ->
+            match head with
+            | test when Regex.IsMatch(test, "^\s") = false ->
+                let newTest = Some (test.Trim(),[])
+                let accumulatedTests = 
+                    match currentTest with
+                    | None -> accumulatedTests
+                    | Some current -> (current::accumulatedTests)
+                loop accumulatedTests newTest tail
+            | variant ->
+                match currentTest with
+                | None -> failwith ("Expecting a test, but given: " + variant)
+                | Some (testName,variants) -> loop accumulatedTests (Some (testName,(variant |> parseVariant)::variants)) tail
+
+    loop [] None (File.ReadAllLines(file) |> List.ofArray)
---
+++ TypeProvTut/Provider.fs
@@ -0,0 +1,128 @@
+﻿module Ttl.Intl.TypeProviders
+
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Core.CompilerServices
+open System.Reflection
+open FileParser
+open System
+open System.IO
+
+type Variant = { Name: string; Test: string; Weight: int; }
+
+[<TypeProvider>]
+type MultivariateProvider (config : TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces ()
+
+    let baseNamespace = "Multivariate.Test"
+    let asm = Assembly.GetExecutingAssembly()
+    let multivariateProvider = 
+        ProvidedTypeDefinition(
+            asm, 
+            baseNamespace, 
+            "MultivariateProvider", 
+            Some(typeof<obj>))
+    let parameters = [ProvidedStaticParameter("PathToNickl", typeof<string>)]
+
+    do multivariateProvider.DefineStaticParameters(parameters, fun typeName args ->
+        let pathToNickl = 
+            let local = args.[0] :?> string
+            Path.Combine([| config.ResolutionFolder ; local |])
+
+        let provider =
+            ProvidedTypeDefinition(
+                asm,
+                baseNamespace,
+                typeName,
+                Some typeof<obj>,
+                HideObjectMethods = true
+            )
+
+        let constructorEmptyParameters exp =
+            ProvidedConstructor(
+                [], 
+                InvokeCode = fun args -> <@@ %exp @@>
+            )
+
+        let configuredTests = parseFile pathToNickl
+
+        let ``testsWithVariantsGreaterThan100%`` = 
+            configuredTests 
+            |> List.map (fun (name, variants) ->
+                (name, variants |> List.map snd |> List.sum > 100)
+            )
+            |> List.filter snd
+
+        let testsWithSameName =
+            configuredTests
+            |> Seq.groupBy fst
+            |> Seq.filter (fun (name, tests) ->
+                tests |> Seq.length > 1
+            )
+
+        let testsWithSameVariantNames =
+            configuredTests
+            |> Seq.filter(fun (_, variants) ->
+                variants
+                |> Seq.groupBy fst
+                |> Seq.exists (fun (_, variants) ->
+                    variants |> Seq.length > 1
+                )
+            )
+
+        let errorGuard tests msg =
+            if not(tests |> Seq.isEmpty)
+            then failwith (sprintf msg (System.String.Join(", ", tests |> Seq.map fst)))
+
+        errorGuard ``testsWithVariantsGreaterThan100%`` "tests %s have variants totalling more than 100 percent" 
+        errorGuard testsWithSameName "duplicate names for tests %s" 
+        errorGuard testsWithSameVariantNames "duplicate variants for tests %s" 
+
+        let addIndividualTests() =
+            for (name, variants) in configuredTests do
+                let testtype = ProvidedTypeDefinition(name, Some typeof<obj>)
+                testtype.HideObjectMethods <- true
+
+                for (variant, weight) in variants do
+                    testtype.AddMember(
+                        ProvidedProperty(
+                            variant, 
+                            typeof<Variant>, 
+                            IsStatic = true,
+                            GetterCode = (fun args -> 
+                                <@@ { Name = variant; Test = name; Weight = weight } @@>)
+                        )
+                    )
+
+                provider.AddMember testtype
+
+        let addAllVariantsProperty() =
+            provider.AddMember(
+                ProvidedProperty(
+                    "AllVariants",
+                    typeof<Variant list>,
+                    IsStatic = true,
+                    GetterCode = fun args -> 
+                        <@@ parseFile pathToNickl
+                            |> List.map(fun (test,variants) -> 
+                                variants 
+                                |> List.map(fun (variant,weight) -> 
+                                    {   
+                                        Name = variant; 
+                                        Test = test;
+                                        Weight = weight }
+                                )
+                            )
+                            |> List.concat @@>
+                )
+            )
+
+        addIndividualTests()
+        addAllVariantsProperty()
+
+        provider
+    )
+
+    do this.AddNamespace(baseNamespace, [multivariateProvider])
+
+[<assembly:TypeProviderAssembly>]
+do ()
